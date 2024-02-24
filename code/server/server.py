ac8b5a1e0d8c064559bbdb617a8e836ee8af98e6
RoteRuebe <yannick.zickler@gmail.c>
2024-02-24 12:20:44 +0100

    Code: Verbesserung Server Flask Server returned jetzt Daten. Diese kommen aus dem deque, welche hier als Ringpuffer verwendet werden Die Daten sind nichtmehr Textfiles sondern JSON
    Client Seitig sind die Requests jetzt asynchron, deswegen braucht es die callback funktionen
    Um nicht 20 callback funktionen zu haben, werden gyro xyz und acc xyz jetzt gleichzeitig abgefragt, was eh mehr sinn macht.
    Das heißt, server returned jetzt 2d array bei /data/gyro oder /acc. ahja statt /log gibt es jetzt /data

--- code/server/server.py
+++ code/server/server.py
@@ -1,24 +1,22 @@
+#import RPi.GPIO as gpio
+#from pyrf24 import *
+from collections import deque
+import threading, flask
 
-import RPi.GPIO as gpio
-from pyrf24 import *
-import sys, time, threading
-from flask import Flask
+### Shared Memory ###
+gyroX = deque([1,2,3,4,5,6,7,8,9,10], maxlen=10)
+gyroY = deque([10,9,8,7,6,5,4,3,2,1], maxlen=10)
+gyroZ = deque([5,5,5,5,5,5,5,5,5,5], maxlen=10)
 
-p_ce = 25
-p_csn = 8
+accX = deque(maxlen=10)
+accY = deque(maxlen=10)
+accZ = deque(maxlen=10)
 
-radio = RF24(25, 0)
-address = b"00002"
+temp = deque(maxlen=10)
+log = deque(maxlen=10)
 
 
-radio.begin()
-radio.openReadingPipe(0, address)
-radio.setPALevel(RF24_PA_MAX)
-
-radio.listen = True
-
-radio.print_details()
-x = 0
+### Rf24 Thread ###
 def write_log(filename, x):
     with open(f"./logs/{filename}.txt", "at") as f:
         f.write( x.split("\x00")[0] + "\n") 
@@ -46,6 +44,17 @@ def process(x):
         write_log("temp", x[1])
 
 def serviceRadio():
+    radio = RF24(25, 0)
+    address = b"00002"
+
+    radio.begin()
+    radio.openReadingPipe(0, address)
+    radio.setPALevel(RF24_PA_MAX)
+
+    radio.listen = True
+
+    radio.print_details()
+
     while True:
         if (radio.available() ):
             rec = radio.read()
@@ -55,10 +64,28 @@ def serviceRadio():
                 process(rec)
             except Exception as e: continue
 
-threading.Thread(target=serviceRadio, args=() ).start()
+#threading.Thread(target=serviceRadio, args=() ).start()
+            
+### Webserver ###
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return "jo"
+    return flask.render_template("index.html", statusRobot="Hello", statusController="World", statusCamera="!")
+
+@app.route("/data/<name>")
+def data(name):
+    if name == "log": return ["Testing environment", "Json Data jeah"]
+    elif name == "temp": return [20]
+    elif name == "gyro": return [list(gyroX), list(gyroY), list(gyroZ)]
+    elif name == "acc": return [list(accX), list(accY), list(accZ)]
+
+    else:
+        return "Error 404 oder so"
+
+if __name__ == "__main__":
+    app.run()
+
+    flask.url_for("static", filename="style.css")
+    flask.url_for("static", filename="source.js")
---
+++ code/server/static/source.js
@@ -0,0 +1,114 @@
+const chart_vibration = new Chart("chart_vibration", {
+    type: "line",
+    data: {
+      labels: [1,2,3,4,5,6,7,8,9,10],
+      datasets: [{
+        borderColor: "blue",
+        data: [],
+        fill: true
+      }]
+    },
+    options: {
+        animation: {duration: 75}
+    }
+  });
+
+const chart_acc = new Chart("chart_acc", {
+    type: "line",
+    data: {
+        labels: [1,2,3,4,5,6,7,8,9,10],
+        datasets: [{
+            borderColor: "red",
+            data: [],
+            fill: true
+        },
+        {
+            borderColor: "blue",
+            data: [],
+            fill: true
+        },
+        {
+            borderColor: "green",
+            data: [],
+            fill: true
+        }
+    ]},
+    options: {
+        animation: {duration: 75}
+    }
+});
+
+const chart_gyro = new Chart("chart_gyro", {
+    type: "line",
+    data: {
+        labels: [1,2,3,4,5,6,7,8,9,10],
+        datasets: [{
+            borderColor: "red",
+            data: [],
+            fill: true
+        },
+        {
+            borderColor: "blue",
+            data: [],
+            fill: true
+        },
+        {
+            borderColor: "green",
+            data: [],
+            fill: true
+        }
+    ]},
+    options: {
+        animation: {duration: 75}
+    }
+});
+
+function sendRequest(url, callback) {
+    let req = new XMLHttpRequest();
+    req.responseType = 'json';
+    req.open("GET", url);
+    req.send();
+
+    // the response is {"message": "Hello, world!"}
+    req.onreadystatechange = function() {
+        if(req.readyState == 4) {
+            callback(req)
+        }
+    };
+}
+
+function callBackLog(req) {
+    log_text = document.getElementById("log_text")
+    log_text.innerHTML = req.response;
+    log_text.scrollTop = log_text.scrollHeight;
+}
+
+function callBackAcc(req) {
+    chart_acc["data"]["datasets"][1]["data"] = req.response[0]
+    chart_acc["data"]["datasets"][2]["data"] = req.response[1]
+    chart_acc["data"]["datasets"][0]["data"] = req.response[2]
+    chart_acc.update()
+}
+
+function callBackGyro(req) {
+    chart_gyro["data"]["datasets"][0]["data"] = req.response[0]
+    chart_gyro["data"]["datasets"][1]["data"] = req.response[1]
+    chart_gyro["data"]["datasets"][2]["data"] = req.response[2]
+    chart_gyro.update()
+}
+
+function getData() {
+    sendRequest("/data/log", callBackLog);
+
+    /*
+    let reqVibration = new XMLHttpRequest();
+    sendRequest(reqVibration, "/data/vibration");
+    */
+    sendRequest("data/gyro", callBackGyro);
+    sendRequest("data/acc", callBackAcc);
+    
+    //chart_vibration["data"]["datasets"][0]["data"] = JSON.parse(reqVibration.response);
+
+    //chart_vibration.update()
+}
+setInterval(getData, 500);
